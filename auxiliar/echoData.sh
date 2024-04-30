9c902aac78b6e10c49c9ab07dfe621f4042859b8
Luis Rodríguez Arriero <lrodriguez@stemdo.io>
2024-04-30 16:55:48 +0200

    Remove var stuff

--- auxiliar/echoData.sh
+++
@@ -1,4 +0,0 @@
-echo "Triggered by a type $GITHUB_REF_TYPE"
-echo "The branch is $GITHUB_REF, with the short name $GITHUB_REF_NAME"
-echo "The operating system on the runner is $RUNNER_OS, name $RUNNER_NAME, WITH ARCHITECTURE $RUNNER_ARCH"
-echo "github base ref is $GITHUB_BASE_REF"
